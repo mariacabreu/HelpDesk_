9986b85cb0b79a6e3c4dd79cec694ce23aa99244
Seu Nome <seuemail@exemplo.com>
2026-04-07 20:26:23 -0300

    saved

--- backend/main.py
+++ backend/main.py
@@ -1085,4 +1085,4 @@ def ler_todas_notificacoes(usuario_id: int, db: Session = Depends(get_db)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
